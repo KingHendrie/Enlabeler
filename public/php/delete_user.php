8f069c6be9d70b49f8c5365577fde17e18fc9f13
Jan Abraham <jandrievisagie@gmail.com>
2023-06-07 19:18:24 +0200

    [wip] delete users

---
+++ public/php/delete_user.php
@@ -0,0 +1,19 @@
+<?php
+	require_once 'db_connection.php';
+
+   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $member_id = $_POST['formName'];
+
+		echo "Received member_id: " . $member_id;
+
+		$query = "DELETE FROM members WHERE member_id = '$member_id'";
+		$result = mysqli_query($conn, $query);
+
+      if ($result) {
+			echo "Succesfully deleted";
+		} else {
+			$error_msg = implode("<br>", $errors);
+			echo '<script>alert("'.$error_msg.'");window.location.href="../html/view_admins.html";</script>';
+		}
+   }
+?>
